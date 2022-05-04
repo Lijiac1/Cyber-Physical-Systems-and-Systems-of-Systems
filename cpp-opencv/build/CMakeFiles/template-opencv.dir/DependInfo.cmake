d0077a4131746cc6041a02ae4643ea2e94652e45
Jiacheng_Li <guslijif@student.gu.se>
2022-05-04 21:42:32 +0200

    implement the local opencv and add the findContours method

---
+++ cpp-opencv/build/CMakeFiles/template-opencv.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/li/桌面/dit638/2022-group-14/cpp-opencv/src/template-opencv.cpp" "/home/li/桌面/dit638/2022-group-14/cpp-opencv/build/CMakeFiles/template-opencv.dir/src/template-opencv.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "."
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
